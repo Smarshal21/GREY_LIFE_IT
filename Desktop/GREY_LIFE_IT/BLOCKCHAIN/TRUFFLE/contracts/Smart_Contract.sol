3342416cc26a0fe8df3463ee2ffee29574353b31
Smarshal <99678760+Smarshal21@users.noreply.github.com>
2024-05-02 16:31:21 +0530

    Update Smart_Contract.sol

--- Desktop/GREY_LIFE_IT/BLOCKCHAIN/TRUFFLE/contracts/Smart_Contract.sol
+++ Desktop/GREY_LIFE_IT/BLOCKCHAIN/TRUFFLE/contracts/Smart_Contract.sol
@@ -60,3 +60,56 @@ contract RiskAssessmentContract is Ownable {
         borrowerData[_borrower].riskMitigationGuidance = _guidance;
     }
 }
+contract ApplicantDataContract {
+    struct Applicant {
+        uint256 id;
+        string name;
+        string jobDescription;
+        uint256 result;
+        uint256 cgpa; 
+    }
+
+    Applicant[] public applicants;
+    uint256 public nextApplicantId;
+
+    function addApplicant(
+        string memory _name,
+        string memory _jobDescription,
+        uint256 _result,
+        uint256 _cgpa
+    ) public {
+        applicants.push(Applicant(nextApplicantId, _name, _jobDescription, _result, _cgpa));
+        nextApplicantId++;
+    }
+
+    function getApplicantsCount() public view returns (uint256) {
+        return applicants.length;
+    }
+
+    function getApplicantById(uint256 _id) public view returns (Applicant memory) {
+        require(_id < applicants.length, "Applicant ID does not exist");
+        return applicants[_id];
+    }
+
+    function updateApplicant(
+        uint256 _id,
+        string memory _name,
+        string memory _jobDescription,
+        uint256 _result,
+        uint256 _cgpa
+    ) public {
+        require(_id < applicants.length, "Applicant ID does not exist");
+        applicants[_id].name = _name;
+        applicants[_id].jobDescription = _jobDescription;
+        applicants[_id].result = _result;
+        applicants[_id].cgpa = _cgpa;
+    }
+
+    function deleteApplicant(uint256 _id) public {
+        require(_id < applicants.length, "Applicant ID does not exist");
+        applicants[_id] = applicants[applicants.length - 1];
+        applicants.pop();
+    }
+}
+
+
